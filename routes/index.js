532bc91b0109936bc6c50b6e72ffef83089688f5
Didier Gbenou <didiergbenou@microsoft.com>
2022-12-12 22:25:41 +0100

    Adding traces

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 const axios = require('axios');
 var Task = require('../models/task');
 
+let appInsights = require("applicationinsights");
+let client = appInsights.defaultClient;
+
 var router = express.Router();
 
 /* GET home page. */
@@ -10,6 +13,8 @@ router.get('/', function(req, res, next) {
     .then((tasks) => {      
       const currentTasks = tasks.filter(task => !task.completed);
       const completedTasks = tasks.filter(task => task.completed === true);
+
+      client.trackTrace({message: "loading home page"});
 
       console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
       res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks, emailServiceResponse: 'Not sent' });
@@ -31,6 +36,8 @@ router.post('/addTask', function(req, res, next) {
   });
   console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
 
+  client.trackTrace({message: `Adding a new task ${taskName} - createDate ${createDate}`});
+
   task.save()
       .then(() => { 
         console.log(`Added new task ${taskName} - createDate ${createDate}`)        
@@ -46,8 +53,10 @@ router.post('/completeTask', function(req, res, next) {
   const taskId = req.body._id;
   const completedDate = Date.now();
 
+  client.trackTrace({message: `Completing task ${taskId}`});
+
   Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now()})
-    .then(() => { 
+    .then(() => {
       console.log(`Completed task ${taskId}`)
       res.redirect('/'); }  )
     .catch((err) => {
@@ -60,6 +69,9 @@ router.post('/completeTask', function(req, res, next) {
 router.post('/deleteTask', function(req, res, next) {
   const taskId = req.body._id;
   const completedDate = Date.now();
+
+  client.trackTrace({message: `Deleting task ${taskId}`});
+
   Task.findByIdAndDelete(taskId)
     .then(() => { 
       console.log(`Deleted task $(taskId)`)      
@@ -83,7 +95,8 @@ router.post('/emailTasks', function(req, res, next){
 
       console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
       
-      console.log("About to send tasks to email service");
+      console.log("About to send tasks to email processor");
+      client.trackTrace({message: "About to send tasks to email processor"});
 
       axios.post(process.env.EMAIL_SERVICE_URL, { 
         emailAddress: emailAddress, 
